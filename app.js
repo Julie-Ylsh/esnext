4b8fe3a99a6b02cf0743e0aa69fe0c6ceecc4fee
julie <julie.bouaye@hotmail.fr>
2019-04-25 11:25:02 +0200

    Finalisation du TP

--- app.js
+++ app.js
@@ -54,10 +54,9 @@ function start() {
             this._price = newPrice;
         }
 
-        static getDefaultId(){return 'rio - de - janeiro'};
-        static getDefaultNom(){return 'Rio de Janeiro'};
-        static getDefaultImageUrl(){return 'img / rio - de - janeiro.jpg'};
-        
+        static getDefaultTrip() {
+            return new Trip('rio-de-janeiro', 'Rio de Janeiro', 'img / rio - de - janeiro.jpg');
+        }
 
     }
 
@@ -69,10 +68,114 @@ function start() {
     parisTrip.price = 100;
     console.log(parisTrip.toString());
 
-    const defaultTrip = getDefaultTrip();
-    console.getDefaultTrip(defaultTrip);
+    const defaultTrip = Trip.getDefaultTrip();
+    console.log(defaultTrip);
 
 
+    //Héritage
+    class FreeTrip extends Trip {
+        constructor(id, name, imageUrl, price) {
+            super(id, name, imageUrl);
+            this.price = 0;
+        }
+
+        toString() {
+            return 'Free' + super.toString();
+        }
+
+    }
+
+    const freeTrip = new FreeTrip('nantes', 'Nantes', 'img/nantes.jpg');
+    console.log(freeTrip.toString());
+
+    //Promise, Set, Map, Arrow Function
+    class TripService {
+        constructor() {
+            // TODO Set of 3 trips
+            this._trips = new Set();
+            this._trips.add(parisTrip);
+            this._trips.add(freeTrip);
+            this._trips.add(defaultTrip);
+        }
+
+        findByName(tripName) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    // ici l'exécution du code est asynchrone
+                    // TODO utiliser resolve et reject en fonction du résultat de la recherche
+                    let tripFound = false;
+                    this._trips.forEach(trip => {
+                        if (trip.name == tripName) {
+                            tripFound = true;
+                            resolve(trip);
+                        }
+                    });
+                    if (!tripFound) {
+                        reject(`No trip with name ${tripName}`);
+                    }
+                }, 2000)
+            });
+        }
+    }
+
+    class PriceService {
+        constructor() {
+            // TODO Map of 2 trips
+            this._priceMap = new Map();
+            this._priceMap.set('paris', 100);
+            this._priceMap.set('rio-de-janeiro', 800);
+        }
+
+        findPriceByTripId(tripId) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    // ici l'exécution du code est asynchrone
+                    // TODO utiliser resolve et reject en fonction du résultat de la recherche
+
+                    if (this._priceMap.has(tripId)) {
+
+                        resolve(this._priceMap.get(tripId));
+
+                    } else {
+                        reject(`No price for trip id ${tripId}`);
+                    }
+                }, 2000)
+            });
+        }
+    }
+
+    const priceService = new PriceService();
+    const tripService = new TripService();
+
+    //Marche
+const promiseParis1$ = tripService.findByName('Paris');
+promiseParis1$
+    .then(trip => console.log(trip))
+    .catch((error) => console.error(`Error : ${error}`));
+
+    //Pas marche
+    const promiseToulouse$ = tripService.findByName('Toulouse');
+    promiseToulouse$
+        .then(trip => console.log(trip))
+        .catch((error) => console.error(`Error : ${error}`));
+
+    // promesse OK
+    const promiseRio$ = priceService.findPriceByTripId('rio-de-janeiro');
+    promiseRio$
+        .then(price => console.log(`Price found : ${price}`))
+        .catch((error) => console.error(`I AM ERROR.\n${error}`));
+
+    // promesse NOK
+    const promiseNantes$ = priceService.findPriceByTripId('nantes');
+    promiseNantes$
+        .then(price => console.log(`Price found : ${price}`))
+        .catch((error) => console.error(`I AM ERROR.\n${error}`));
+
+    const promiseParis$ = tripService.findByName('Paris');
+    promiseParis$
+        .then(trip => priceService.findPriceByTripId(trip.id))
+        .then(price => console.log('Price found :', price))
+        .catch((error) => console.error(`I AM ERROR.\n${error}`));
 
 }
 
